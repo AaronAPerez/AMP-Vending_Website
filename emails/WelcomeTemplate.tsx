869e31c0469be2b9897ac4b0c1174125bda12da6
AaronAPerez <aaperez06@gmail.com>
2025-04-17 08:54:03 -0700

    removed contact route, welcome template

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,28 +1,28 @@
-import { Body, Container, Html, Preview } from '@react-email/components'
-import Link from 'next/link'
-import React from 'react'
+// import { Body, Container, Html, Preview } from '@react-email/components'
+// import Link from 'next/link'
+// import React from 'react'
 
-const WelcomeTemplate = () => {
-    return (
-        <>
-            <Html>
+// const WelcomeTemplate = () => {
+//     return (
+//         <>
+//             <Html>
 
-                <Preview>Welcome to our service!</Preview>
-                <Body>
-                    <Container className="bg-white text-black mx-auto p-4 max-w-2xl">
-                        <h1 className="text-2xl font-bold">Welcome!</h1>
-                        <p className="mt-4">Thank you for signing up for our service.</p>
-                        <p className="mt-2">We are excited to have you on board!</p>
-                        <p className="mt-4">Best regards,</p>
-                        <p>Your Company</p>
-                        <p className="mt-2">If you have any questions, feel free to reach out.</p>
-                        <Link href="https://amp-vending-website.vercel.app/" className="text-blue-500 underline mt-4">Visit our website</Link>
-                    </Container>
-                </Body>
+//                 <Preview>Welcome to our service!</Preview>
+//                 <Body>
+//                     <Container className="bg-white text-black mx-auto p-4 max-w-2xl">
+//                         <h1 className="text-2xl font-bold">Welcome!</h1>
+//                         <p className="mt-4">Thank you for signing up for our service.</p>
+//                         <p className="mt-2">We are excited to have you on board!</p>
+//                         <p className="mt-4">Best regards,</p>
+//                         <p>Your Company</p>
+//                         <p className="mt-2">If you have any questions, feel free to reach out.</p>
+//                         <Link href="https://amp-vending-website.vercel.app/" className="text-blue-500 underline mt-4">Visit our website</Link>
+//                     </Container>
+//                 </Body>
 
-            </Html>
-        </>
-    )
-}
+//             </Html>
+//         </>
+//     )
+// }
 
-export default WelcomeTemplate
+// export default WelcomeTemplate
